a4ea573721a6611b2865f733294283a83e1a789a
MadKara <madkara13@gmail.com>
2022-08-01 22:19:22 +0300

    Added packages, pom file, modified gitignore and added tempo files

---
+++ src/main/java/com/company/pageobjects/MainPage.java
@@ -0,0 +1,7 @@
+package com.company.pageobjects;
+
+public class MainPage {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/company/MainTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void verifyMainTest() {
+    }
+}
